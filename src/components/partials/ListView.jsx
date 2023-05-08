fc2e39f043f582462da9fe856cc92448987b2223
awellsbiz <awellsbiz@gmail.com>
2023-05-08 11:38:02 -0700

    fixed error

--- src/components/partials/ListView.jsx
+++ src/components/partials/ListView.jsx
@@ -6,14 +6,14 @@ export default function ListView({ movies }) {
   return (
     <div className="list-view">
       <div className="list-view">
-        {movies.map((movie) => (
-          <>
+        {movies.map((movie, i) => (
+          <div key={`movie.id:${i}`}>
             {movie.poster_path ?
               <Card key={movie.id} movie={movie} />
               :
               <></>
             }
-          </>
+          </div>
         ))}
       </div>
     </div>
